adfae39d23490ae8585f9c4372870c1decb7340a
ahmadUffi <ahmaduffi45@gmail.com>
2025-07-31 11:45:32 +0700

    feat: add Login and Register components with form handling and validation

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,4 +1,5 @@
 import logo from "./logo.png";
 import backgroundRegister from "./backgroundRegister.png";
+import maskot from "./Maskot.png";
 
-export { logo, backgroundRegister };
+export { logo, backgroundRegister, maskot };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,6 +9,26 @@ export default {
         "custom-green": "#f3fff4",
         "custom-pink": "#ffeded",
       },
+      animation: {
+        float: "float 6s ease-in-out infinite",
+        "float-delayed": "float 6s ease-in-out infinite 3s",
+        shine: "shine 2s linear infinite",
+        glow: "glow 2s ease-in-out infinite alternate",
+      },
+      keyframes: {
+        float: {
+          "0%, 100%": { transform: "translateY(0px)" },
+          "50%": { transform: "translateY(-20px)" },
+        },
+        shine: {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(100%)" },
+        },
+        glow: {
+          "0%": { boxShadow: "0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff" },
+          "100%": { boxShadow: "0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff" },
+        },
+      },
     },
   },
   plugins: [],
