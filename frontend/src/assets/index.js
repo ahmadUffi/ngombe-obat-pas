f765cb8ab185a6f1821ed2478d1850d766f1b13b
ahmadUffi <ahmaduffi45@gmail.com>
2025-09-03 15:21:40 +0700

    refactor: Remove unused backgroundRegister image and update asset exports fixv4

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,5 +1,4 @@
 import logo from "./logo.png";
-import backgroundRegister from "./backgroundRegister.png";
 import maskot from "./Maskot.png";
 
-export { logo, backgroundRegister, maskot };
+export { logo, maskot };
